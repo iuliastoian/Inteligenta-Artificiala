bb0160ded9da83ee95c31bde0ba376d0e1a5d1df
iuliastoian <stoianiuliatia@gmail.com>
2020-10-06 22:22:32 +0300

    Create tutorial-4.py

---
+++ ia-lab-1/tutorial-4.py
@@ -0,0 +1,23 @@
+# command for creating executable python file: python main.py
+# change rights so that all users can execute the file: chmod a+x main.py
+# execute the file: ./main.py
+
+# age = int(raw_input("How old are you? "))
+
+"""if (age > 0) and (age < 120):
+    if (age == 21):
+        print "Same as me"
+    elif (age > 21):
+        print "Older than me"
+    else:
+        print "Younger than me"
+else:
+    print "Don't lie about your age"""""
+
+# Conditional Expresion
+
+x, y = 1, 0
+
+a = 'y is less than x' if (y < x) else 'x is less than y'
+
+print a
